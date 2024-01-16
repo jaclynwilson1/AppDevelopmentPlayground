bf1c3231449b2890228cb80d56a3a5640bb9862d
wilsonjacyn1 <17icaj@gmail.com>
2024-01-16 14:59:58 -0500

    Stack View Demo

---
+++ StackViewDemo/StackViewDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StackViewDemo
+//
+//  Created by Jaclyn Wilson on 1/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
