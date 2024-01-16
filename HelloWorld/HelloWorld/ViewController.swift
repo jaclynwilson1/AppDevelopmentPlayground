2c4bfb36719a11e951d71c79ee11ce75b908ff07
wilsonjacyn1 <17icaj@gmail.com>
2024-01-16 14:47:12 -0500

    Meaning for emojis

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -13,11 +13,23 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
     //add to the button for hello world - jwil
     @IBAction func showMessage(sender: UIButton) {
-        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
-        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-        present(alertController, animated: true, completion: nil)
+        //dictionary for emojis
+        var emojiDict = ["👻": "Ghost", "👾": "Monster Man", "Hello World": "Hello World"]
+        
+        let selectedButton = sender
+        
+        if let wordToLookup = selectedButton.titleLabel?.text {
+            
+            var meaning = emojiDict[wordToLookup]
+            
+            let alertController = UIAlertController(title: "Meaning", message: meaning, preferredStyle: UIAlertController.Style.alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            present(alertController, animated: true, completion: nil)
+        }
     }
     
 
